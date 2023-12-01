607898d6d7a5d4740a42e8d9690cadf0acdb7d76
anshulsoni4 <anshulsoni3040@gmail.com>
2023-12-01 13:51:40 +0530

    first commit

---
+++ image-ai/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+// import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import Image from './components/image-gen/Image.jsx';
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    
+      <div>
+        <Image />
+      </div>
+    
+  )
+}
+
+export default App
